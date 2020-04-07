3b3827e242f768d7899b3c88dfe448caa68084e8
Tuấn Anh <vtanh1905@gmail.com>
2020-04-07 14:59:26 +0700

    change entryPoint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { registerRootComponent } from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test Redux with React native</Text>
     </View>
   );
 }
@@ -17,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+registerRootComponent(App);
